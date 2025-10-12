3cd519141f72eddab77a58afac271825b97463af
ZainC0330 <zainchowdhry4@gmail.com>
2025-10-12 04:03:48 -0400

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import "../global.css";
+
+const Home = () => {
+    
+    return(
+        <View>
+            <Text className="text-xl font-bold text-blue-500">Donkey Kong</Text>
+        </View>
+    )
+}
+
+export default Home;
+
+const styles = StyleSheet.create({});
